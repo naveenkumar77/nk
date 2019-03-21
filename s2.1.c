81303b08f54d163305973cbdd821a82818be2ea8
naveenkumar77 <48518973+naveenkumar77@users.noreply.github.com>
2019-03-21 19:01:20 +0530

    Create s2.1.c

---
+++ s2.1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+voidmain()
+{
+	char a[10];
+	scanf("%s",a);
+	if(a[0]=='S'||a[0]=='s')
+	{
+	 printf("yes...holiday");}
+	else
+	{
+	 printf("no..not a holiday");}
+	return 0;}
